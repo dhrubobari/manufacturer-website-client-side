c403b675f3e1149a1136022963806adbd4b7617d
dhrubobari <bari.dhrubo@gmail.com>
2022-05-26 10:03:47 +0600

    navbar & banner added

---
+++ src/components/Home/Banner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import toolbox from "../../toolbox.jpeg";
+
+const Banner = () => {
+  return (
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content flex-col lg:flex-row-reverse">
+        <img
+          src={toolbox}
+          class="max-w-sm rounded-lg shadow-xl"
+        />
+        <div>
+          <h1 class="text-4xl font-medium">INNOVATIVE EQUIPMENTS</h1>
+          <p class="py-6">
+          Tools and equipment are two words that are often used synonymously, mainly due to the similarities of their meanings. Because of this factor, they can be used interchangeably in most instances.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
